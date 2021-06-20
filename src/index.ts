8017aa1732a63f1fb40f2d541fe307cbbe25e1c7
ericoandre <ericoandresilva@gmail.com>
2021-06-20 10:30:03 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import 'reflect-metadata';
-import {connecte} from './config/typeorm';
+import { createConnection } from "typeorm";
 
 import { ApolloServer } from 'apollo-server';
 import { getSchema } from './graphql'
 
-export async function runServer() {
+import path from 'path';
+
+const main = async () => {
+    await createConnection(
+        {
+            type: "sqlite",
+            database: path.join(__dirname, "../", "db.sqlite3"),
+            synchronize: true,
+            entities: [path.join(__dirname, "./entity", "*")],
+        }
+    );
+
     const schema = await getSchema();
     const server = new ApolloServer({ schema });
+
     await server.listen(process.env.PORT || 4000).then(({ url }) => {
         console.log(`🚀 Server ready at ${url}`);
     });
-}
+};
 
-connecte();
-runServer();
+main();
